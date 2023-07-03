38549d91cc6ab24637a9f1782f325f672321da6b
sourav-behera <souravdipu2002@gmail.com>
2023-07-04 01:56:23 +0530

    feat: added vscode like block navigation alt-j/k

--- lua/configs/keybinds.lua
+++ lua/configs/keybinds.lua
@@ -2,26 +2,26 @@
 -- Define the keybindings
 local keybindings = {
   -- Insert mode mappings
-  { mode = 'i', lhs = '<C-h>', rhs = '<Left>' },
-  { mode = 'i', lhs = '<C-j>', rhs = '<Down>' },
-  { mode = 'i', lhs = '<C-k>', rhs = '<Up>' },
-  { mode = 'i', lhs = '<C-l>', rhs = '<Right>' },
+  { mode = 'i', shortcut = '<C-h>', command = '<Left>' },
+  { mode = 'i', shortcut = '<C-j>', command = '<Down>' },
+  { mode = 'i', shortcut = '<C-k>', command = '<Up>' },
+  { mode = 'i', shortcut = '<C-l>', command = '<Right>' },
 
   -- VSCode like block navigation
   -- Normal mode mappings
-  { mode = 'n', lhs = '<A-j>', rhs = ':m .+1<CR>==' },
-  { mode = 'n', lhs = '<A-k>', rhs = ':m .-2<CR>==' },
+  { mode = 'n', shortcut = '<A-j>', command = ':m .+1<CR>==' },
+  { mode = 'n', shortcut = '<A-k>', command = ':m .-2<CR>==' },
 
   -- Insert mode mappings
-  { mode = 'i', lhs = '<A-j>', rhs = '<Esc>:m .+1<CR>==gi' },
-  { mode = 'i', lhs = '<A-k>', rhs = '<Esc>:m .-2<CR>==gi' },
+  { mode = 'i', shortcut = '<A-j>', command = '<Esc>:m .+1<CR>==gi' },
+  { mode = 'i', shortcut = '<A-k>', command = '<Esc>:m .-2<CR>==gi' },
 
   -- Visual mode mappings
-  { mode = 'x', lhs = '<A-j>', rhs = ":m'>+1<CR>gv=gv" },
-  { mode = 'x', lhs = '<A-k>', rhs = ":m'<-2<CR>gv=gv" },
+  { mode = 'x', shortcut = '<A-j>', command = ":m'>+1<CR>gv=gv" },
+  { mode = 'x', shortcut = '<A-k>', command = ":m'<-2<CR>gv=gv" },
 }
 
 -- Apply the keybindings
 for _, mapping in ipairs(keybindings) do
-  vim.api.nvim_set_keymap(mapping.mode, mapping.lhs, mapping.rhs, { noremap = true, silent = true })
+  vim.api.nvim_set_keymap(mapping.mode, mapping.shortcut, mapping.command, { noremap = true, silent = true })
 end
