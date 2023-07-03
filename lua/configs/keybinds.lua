f540104c65ed3d8b1adcc57727da3ffd10506422
sourav-behera <souravdipu2002@gmail.com>
2023-07-04 01:56:23 +0530

    feat: added vscode like block navigation alt-j/k

--- lua/configs/keybinds.lua
+++ lua/configs/keybinds.lua
@@ -4,3 +4,15 @@ vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true })
 
+-- Normal mode mappings
+vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+
+-- Insert mode mappings
+vim.api.nvim_set_keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true })
+
+-- Visual mode mappings
+vim.api.nvim_set_keymap('x', '<A-j>', ":m'>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', '<A-k>', ":m'<-2<CR>gv=gv", { noremap = true, silent = true })
+
